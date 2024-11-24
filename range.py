49e76351d3e126a74e90a3ec014e8c42f112d214
brajesh708 <brajeshmewada694@gmail.com>
2024-11-24 11:51:27 +0530

    tebal

--- range.py
+++ range.py
@@ -1,7 +1,17 @@
-for a in range(1,11):
-    print(a)
+# for a in range(1,11):
+#     print(a)
 
 
 
-for a in range(5):
-    print("brajesh")
+# for a in range(5):
+#     print("brajesh")
+    
+# for a in range(1,11,2):
+#     print(a)
+
+
+for a in range(1,11,1):
+    print(a*a)
+    
+for a in range(1,11,1):
+    print(a*2)
