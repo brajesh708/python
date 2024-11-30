37e7fcd0692d281bc6cf3c27e75e777981c25f1b
brajesh708 <brajeshmewada694@gmail.com>
2024-11-30 23:11:22 +0530

    new

--- string.py
+++ string.py
@@ -4,3 +4,16 @@ print(id(a))
 print(type(a))
 
 print(a[0::2])
+print(a[1::2])
+
+str1 = "brajesh"
+
+print(str1[::-1])
+
+str2 = "hello"
+print(str2[0:3])
+
+str3 = "hello world"
+print(str3[0:5])
+
+        
