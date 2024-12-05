ef0e07c8a16eb719ac7860ce62010dd1611ba8c4
brajesh708 <brajeshmewada694@gmail.com>
2024-12-05 17:00:39 +0530

    update

--- loop.py
+++ loop.py
@@ -10,3 +10,19 @@ s = "Hello, World!"
 
 # Expected output: 5
 print(len(s))
+
+
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello Geek")
+else:
+    print("In Else Block")
+
+for i in range(3):
+    print("Hello Geek")
+
+    
+print("Test case 2: Strings with multiple words")
+
+s = "Hello World!"
