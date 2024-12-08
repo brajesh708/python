0179ee0305800466a47706f3a3c85561696a276b
brajesh708 <brajeshmewada694@gmail.com>
2024-12-08 23:06:25 +0530

    new

--- whileloop.py
+++ whileloop.py
@@ -8,3 +8,13 @@ a=10
 while a>=1:
     print(a,"hello")
     a=a-1
+    
+print("Test case 2: Strings with multiple words")
+
+s = "Hello World!"
+print(s)  # Output: Hello World!
+print(s[0])  # Output: H
+
+print(s[0::2])  # Output: Hlo
+
+print(s[1::2])  # Output: eWl
