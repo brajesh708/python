f066678ecb8425dffee5b3d49ecdaebb2da403da
brajesh708 <brajeshmewada694@gmail.com>
2024-12-08 23:05:12 +0530

    new

--- secend.py
+++ secend.py
@@ -3,3 +3,10 @@ from functools import reduce
 numbers = [1, 3, 5, 2, 4]
 max_value = reduce(lambda x, y: x if x > y else y, numbers)
 print(max_value)  # Output: 5
+
+print(reduce(lambda x, y: x + y, numbers))  # Output: 15
+
+print(reduce(lambda x, y: x * y, numbers))  # Output: 60
+
+print(reduce(lambda x, y: x - y, numbers))  # Output: -12
+
