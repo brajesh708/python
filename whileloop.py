b335909c5685ae77e0d6765a7e5143d7f0f3ab95
brajesh708 <brajeshmewada694@gmail.com>
2024-12-11 23:16:26 +0530

    new

--- whileloop.py
+++ whileloop.py
@@ -18,3 +18,18 @@ print(s[0])  # Output: H
 print(s[0::2])  # Output: Hlo
 
 print(s[1::2])  # Output: eWl
+
+
+# some more functions
+def is_prime(n):
+    if n in [2, 3]:
+        return True
+    if (n == 1) or (n % 2 == 0):
+        return False
+    r = 3
+    while r * r <= n:
+        if n % r == 0:
+            return False
+        r += 2
+    return True
+print(is_prime(78), is_prime(79))
