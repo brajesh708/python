8eb10fae9f548afecb08df8e8a3d02532645bc73
brajesh708 <brajeshmewada694@gmail.com>
2024-12-22 22:39:56 +0530

    first

--- ws.py
+++ ws.py
@@ -1,22 +1,27 @@
-a=10
-b=4
+# a=10
+# b=4
 
-def add(a,b):
-    return a+b
+# def add(a,b):
+#     return a+b
 
-def subtract(a,b):
-    return a-b
+# def subtract(a,b):
+#     return a-b
 
-def multiply(a,b):
-    return a*b
+# def multiply(a,b):
+#     return a*b
 
-def divide(a,b):
-    if b==0:
-        return "Error: Division by zero is not allowed"
-    else:
-        return a/b
+# def divide(a,b):
+#     if b==0:
+#         return "Error: Division by zero is not allowed"
+#     else:
+#         return a/b
 
-print("Addition:", add(a,b))
-print("Subtraction:", subtract(a, b))
-print("Multiplication:", multiply(a, b))
-print("Division:", divide(a, b))
+# print("Addition:", add(a,b))
+# print("Subtraction:", subtract(a, b))
+# print("Multiplication:", multiply(a, b))
+# print("Division:", divide(a, b))
+
+
+print("heelo world")
+
+
