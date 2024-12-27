6f03c9bdf9687b40098a8436c7baa9592accab95
brajesh708 <brajeshmewada694@gmail.com>
2024-12-27 21:26:19 +0530

    update

--- new.py
+++ new.py
@@ -45,3 +45,15 @@ print(add_tuples(tup1, tup2))
 #     return tup[::-1]
 
 # print(reverse_tuple(tup1))
+
+
+tup1=(1, "a" , True)
+tup2=(2,4,6)
+print(tup1+tup2)
+
+def add_tuples(tup1, tup2):
+    return (tup1[0] + tup2[0], tup1[1]
+            + tup2[1], tup1[2] + tup2[2])
+    
+    
+print(add_tuples(tup1, tup2))
